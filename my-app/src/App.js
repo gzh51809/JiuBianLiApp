08f742e16063ab5d875b77c9f8a091f958813bca
y-haitao <851382616@qq.com>
2019-01-25 15:13:34 +0800

    今天也是充满希望的一天

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//引入Ant Design 插件
+import {Button} from 'antd';
 
 class App extends Component {
   render() {
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Button type="primary">今天也是充满希望的一天</Button>
       </div>
     );
   }
